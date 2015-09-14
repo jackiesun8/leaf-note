36a578713f33d10ef812f066d829dd0de749eee4
jackiesun <jackiesun8@yeah.net>
2015-09-14 20:22:52 +0800

    up

--- leaf/module/skeleton.go
+++ leaf/module/skeleton.go
@@ -3,7 +3,7 @@ package module
 import (
 	"github.com/name5566/leaf/chanrpc"
 	"github.com/name5566/leaf/console"
-	"github.com/name5566/leaf/go"
+	"github.com/name5566/leaf/go" //包名实际为g
 	"github.com/name5566/leaf/log"
 	"github.com/name5566/leaf/timer"
 	"time"
